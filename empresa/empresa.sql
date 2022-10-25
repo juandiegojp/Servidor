bb9fc93303fbee2227282e0402cae213667c9122
Juan Diego <juandiego.jurado@iesdonana.org>
2022-10-25 20:20:58 +0200

    Tabla: Empleados

--- empresa/empresa.sql
+++ empresa/empresa.sql
@@ -6,11 +6,48 @@ CREATE TABLE departamentos (
     denominacion varchar(255) NOT NULL
 );
 
--- Fixtures:
+DROP TABLE IF EXISTS empleados CASCADE;
 
+CREATE TABLE empleados (
+    id bigserial PRIMARY KEY,
+    numero numeric(4) NOT NULL UNIQUE,
+    nombre varchar(255) NOT NULL,
+    salario numeric(7, 2) NOT NULL,
+    fecha_nac timestamp NOT NULL,
+    departamento_id bigint NOT NULL REFERENCES departamentos (id)
+);
+
+-- Fixtures:
 INSERT INTO departamentos (codigo, denominacion)
-    values (10, 'informática'),
-            (20, 'administrativo'),
-            (30, 'Prevención'),
-            (40, 'Laboratorio'),
-            (50, 'Automoción');
+    VALUES (10, 'informática'), (20, 'administrativo'), (30, 'Prevención'), (40, 'Laboratorio'), (50, 'Automoción');
+
+INSERT INTO empleados (numero,
+    nombre,
+    salario,
+    fecha_nac,
+    departamento_id) (
+VALUES (1,
+    'Antonio',
+    975.85,
+    '1993-02-27',
+    1),
+(2,
+    'Lidhia',
+    1320,
+    '1991-01-06',
+    2),
+(3,
+    'Juan',
+    821.62,
+    '1996-05-28',
+    3),
+(4,
+    'Joanna',
+    1375.50,
+    '1997-10-01',
+    4),
+(5,
+    'Cthulu',
+    1950.99,
+    '1920-11-01',
+    5) );
